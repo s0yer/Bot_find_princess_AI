d06e9f0094b5cb5cde1b72f01730b77771370f9d
jadsonmarliere <jds.mdo@gmail.com>
2019-11-19 09:21:14 -0300

    update

--- application.py
+++ application.py
@@ -1,8 +1,17 @@
 
 
+
+
+# Size of the Matrix
 input(n)
 
+# Constraint of 'n', n is a integer
 if 3 <= n <= 100:
+
+    # 'm' in the Center of the odd Matrix
+    i = (round(n / 2))
+    j = i
+    # m[i, j]
 
 else:
     print("Input a value between [3,100] and odd.")
